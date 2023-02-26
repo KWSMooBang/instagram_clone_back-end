7a8e70f71c1b50437ff915d00ee73a6ff5b1211b
KWSMooBang <40kimwoosung@gmail.com>
2023-02-26 21:34:19 +0900

    create SpringBoot Project

---
+++ src/main/kotlin/com/example/server/InstagramCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.example.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InstagramCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<InstagramCloneApplication>(*args)
+}
